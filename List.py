8359c08431d6c158113722e36481588318b72750
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2025-11-16 21:57:20 +0530

    Update List.py

--- List.py
+++ List.py
@@ -7,6 +7,9 @@ print(a[0])
 
 # Accessing the last item
 print(a[-1])
+# output
+# 10
+# 50
 
 # Using List Slicing
 
@@ -14,3 +17,5 @@ a = [10, 20, 30, 40, 50]
 
 # Get a slice of the list from index 1 to 3
 print(a[1:4])
+# Output 
+# 20,30,40
